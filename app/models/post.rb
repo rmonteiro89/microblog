37b29e9561d9b63228121274e70fc88113c4cbf3
Raphael Monteiro <rmonteiro89@hotmail.com>
2014-07-31 20:05:11 -0300

    Requisitos do Projeto done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
 	validates :title, presence: true
 	validates :message, presence: true
+	
+	def to_param
+	  title.parameterize				#Parametro da url no formato slugify
+	end
 end
