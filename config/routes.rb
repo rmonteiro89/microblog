51e9a1611130725c4e39b041180189373386ee3e
Raphael Monteiro <rmonteiro89@hotmail.com>
2014-07-31 17:01:42 -0300

    Controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  
+  match '/new', to: 'posts#new', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
